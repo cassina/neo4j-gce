e501a7464ba68ff22897c15cfd90b41acb12756f
Bernardo Cassina Buerba <bernardocassinabuerba@Bernardos-MacBook-Pro.local>
2016-08-26 05:15:44 -0500

    Updated for neo4j.conf.

--- start.py
+++ start.py
@@ -21,7 +21,7 @@ from googleapiclient import discovery
 class Neo4jClusterService:
     project = "novelistik-sb"
     zone = "us-central1-f"
-    group = "n4j-cluster"
+    group = "neo4j-cluster"
     n4j_path = "/neo4j"
     n4j_home_path = n4j_path + "/neo4j-enterprise-3.0.3"
     tmp_path = n4j_path + "/temp.conf"
@@ -29,18 +29,22 @@ class Neo4jClusterService:
     backup_script = "/neo4j-enterprise-3.0.3/bin/neo4j-backup"
     backup_path = n4j_path + '/backup/graph.db'
     id_url = "http://metadata.google.internal/computeMetadata/v1/instance/id"
+    ip_url = "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip"
     # id_ulr = "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip"
 
     ha_dbms_connector_http = "#dbms.connector.http.address=0.0.0.0:7474"
     ha_server_id = "#ha.server_id="
     ha_initial_hosts = "#ha.initial_hosts=127.0.0.1:5001,127.0.0.1:5002,127.0.0.1:5003"
     ha_dbms_mode = "#dbms.mode=HA"
+    ha_status_auth = "#ha_status_auth_enabled=false"
+    ha_host_data = " #ha.host.data:127.0.0.1:6001"
+    ha_host_coord = "#ha.host.coordination:127.0.0.1:5001"
 
     def __init__(self):
         self.msg(s="New Neo4j Service Initialization")
-        i_id, ips = self.get_parse_data()
+        i_id, ips, server_ip = self.get_parse_data()
         self.msg(s="Got parsed data {i_id} {ips}".format(i_id=i_id, ips=ips))
-        self.update_neo4j_conf(i_id=i_id, ips=ips)
+        self.update_neo4j_conf(i_id=i_id, ips=ips, server_ip=server_ip)
 
     @classmethod
     def msg(cls, s):
@@ -79,10 +83,12 @@ class Neo4jClusterService:
         gce = discovery.build("compute", "v1", credentials=credentials)
         return gce
 
-    def get_instance_id(self):
+    def get_instance_info(self):
         headers = {"Metadata-Flavor": "Google"}
-        result = requests.get(self.id_url, headers=headers)
-        return str(result.text[0:6])
+        server_id = requests.get(self.id_url, headers=headers)
+        server_ip = requests.get(self.ip_url, headers=headers)
+
+        return str(server_id.text[0:6]), str(server_ip)
 
     def get_running_vms(self, compute):
         body = {"instanceState": "RUNNING"}
@@ -100,23 +106,29 @@ class Neo4jClusterService:
         return result
 
     def get_parse_data(self):
-        i_id = self.get_instance_id()
+        i_id, server_ip = self.get_instance_info()
         gce = self.create_gc_services()
         ips = self.get_running_vms(compute=gce)
-        return i_id, ips
 
-    def update_neo4j_conf(self, i_id, ips):
+        return i_id, ips, server_ip
+
+    def update_neo4j_conf(self, i_id, ips, server_ip):
 
         self.msg(s="Updating neo4j.conf")
 
         server_id = "ha.server_id={instance_id}".format(instance_id=i_id)
         initial_hosts = "ha.initial_hosts={hosts}".format(hosts=ips)
+        host_data = server_ip + ":6001"
+        host_coord = server_ip + ":5001"
 
         replace_data = {
             self.ha_server_id: server_id,
             self.ha_initial_hosts: initial_hosts,
             self.ha_dbms_connector_http: self.uncomment_line(s=self.ha_dbms_connector_http),
-            self.ha_dbms_mode: self.uncomment_line(s=self.ha_dbms_mode)
+            self.ha_dbms_mode: self.uncomment_line(s=self.ha_dbms_mode),
+            self.ha_status_auth: self.uncomment_line(s=self.ha_status_auth),
+            self.ha_host_data: host_data,
+            self.ha_host_coord: host_coord
         }
 
         self.write_neo4j_conf(replace_dict=replace_data)
